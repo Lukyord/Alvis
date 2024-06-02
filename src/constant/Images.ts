6d323b8a628a622fd4b92b803d60aa62a644a009
Kongtup <ttansiri524@gmail.com>
2024-06-02 11:44:19 +0700

    📦 add tailwind prettier
    📦 remove unused resources, init image importing object

---
+++ src/constant/Images.ts
@@ -0,0 +1,88 @@
+// ========== File Naming Conventions ==========
+// [IllustrationName]-#[Detail]-[IconColor].svg e.g. chevron-down-black.svg, arrow-curve-up-white.svg
+
+// ========== Illustration Naming Conventions ==========
+// [IllustrationName + Detail][IconColor] e.g. ChevronDownBlack, ArrowCurveUpWhite
+
+// A
+// B
+// C
+// D
+// E
+// F
+// G
+// H
+// I
+// J
+// K
+// L
+// M
+// N
+// O
+// P
+// Q
+// R
+// S
+// T
+// U
+// V
+// W
+// X
+// Y
+// Z
+
+export const Images = {
+  // A
+  // B
+  // C
+  // D
+  // E
+  // F
+  // G
+  // H
+  // I
+  // J
+  // K
+  // L
+  // M
+  // N
+  // O
+  // P
+  // Q
+  // R
+  // S
+  // T
+  // U
+  // V
+  // W
+  // X
+  // Y
+  // Z
+}
+
+// A
+// B
+// C
+// D
+// E
+// F
+// G
+// H
+// I
+// J
+// K
+// L
+// M
+// N
+// O
+// P
+// Q
+// R
+// S
+// T
+// U
+// V
+// W
+// X
+// Y
+// Z
