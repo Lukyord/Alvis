71503d5f2ca7041a825176d821646f034a1d5ef3
Natthawut Saexu <xutiger2001@gmail.com>
2024-06-04 17:45:47 +0700

    🍕 edit order , rule , type in catalogue

--- sanity/schemaTypes/catalogue.js
+++ sanity/schemaTypes/catalogue.js
@@ -14,7 +14,7 @@ export default {
       title: 'Main Image',
       type: 'image',
       options: {
-        accept: '.jpg,.jpeg,.png',
+        accept: 'image/*',
         storeOriginalFilename: true,
       },
       validation: (rule) => rule.required(),
@@ -30,7 +30,7 @@ export default {
             {
               name: 'model',
               title: 'Model',
-              type: 'number',
+              type: 'string',
               validation: (rule) => rule.required(),
             },
             {
@@ -42,41 +42,40 @@ export default {
             {
               name: 'area',
               title: 'Area (sq.m.)',
-              type: 'string',
-              validation: (rule) =>
-                rule
-                  .required()
-                  .custom((value) => {
-                    const regex = /^\d+(\.\d+)?m\s*x\s*\d+(\.\d+)?m$/;
-                    if (!regex.test(value)) {
-                      return 'Please provide the area in the format "length (m) x width (m)", for example: 16.5m x 18.5m';
-                    }
-                    return true;
-                  }),
+              type: 'number',
+              validation: (rule) => rule.required().positive(),
             },
             {
               name: 'land_area_require',
-              title: 'Land Area Require ',
-              type: 'number',
-              validation: (rule) => rule.required().positive(),
+              title: 'Land Area Requirement',
+              type: 'string',
+              description: 'Please provide the area in the format "length(m) x width(m)" e.g., 16.5m x 18.5m',
+              validation: (rule) =>
+                rule.required().custom((value) => {
+                  const regex = /^\d+(\.\d+)?m\s*x\s*\d+(\.\d+)?m$/
+                  if (!regex.test(value)) {
+                    return 'Area does not match the required format'
+                  }
+                  return true
+                }),
             },
             {
               name: 'bedroom',
               title: 'Bedroom ',
               type: 'number',
-              validation: (rule) => rule.required().positive(),
+              validation: (rule) => rule.required().positive().integer(),
             },
             {
               name: 'bathroom',
               title: 'Bathroom ',
               type: 'number',
-              validation: (rule) => rule.required().positive(),
+              validation: (rule) => rule.required().positive().integer(),
             },
             {
               name: 'nulti_purpose_area',
               title: 'Multi-Purpose Area ',
               type: 'number',
-              validation: (rule) => rule.required().positive(),
+              validation: (rule) => rule.required().positive().integer(),
             },
             {
               name: 'price',
