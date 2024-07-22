9f6b129ba574fa1d20275db71ec246c92c10067d
Natthawut Saexu <xutiger2001@gmail.com>
2024-07-22 22:04:49 +0700

    🍕 Add slug & Edit portfolio

--- sanity/schemaTypes/catalogue.js
+++ sanity/schemaTypes/catalogue.js
@@ -10,6 +10,16 @@ export default {
       validation: (Rule) => Rule.required(),
     },
     {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (rule) => rule.required(),
+    },
+    {
       name: 'heroImage',
       title: 'Hero Image',
       type: 'image',
--- sanity/schemaTypes/portfolio.js
+++ sanity/schemaTypes/portfolio.js
@@ -12,21 +12,16 @@ export default {
     {
       name: 'slug',
       type: 'slug',
-      title: 'Slug of your portfolio',
+      title: 'Slug',
       options: {
         source: 'title',
+        maxLength: 96,
       },
       validation: (rule) => rule.required(),
     },
     {
-      name: 'short_description',
-      title: 'Short Description',
-      type: 'text',
-      validation: (rule) => rule.required().max(180),
-    },
-    {
-      name: 'main_image',
-      title: 'Main Image',
+      name: 'heroImage',
+      title: 'Hero Image',
       type: 'image',
       options: {
         hotspot: true,
@@ -34,6 +29,18 @@ export default {
         storeOriginalFilename: true,
       },
       validation: (rule) => rule.required(),
+    },
+    {
+      name: 'styleName',
+      title: 'Style Name',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    },
+    {
+      name: 'size',
+      title: 'Size (sq.m)',
+      type: 'string',
+      validation: (rule) => rule.required().positive(),
     },
     {
       name: 'location',
@@ -51,37 +58,137 @@ export default {
       validation: (rule) => rule.required(),
     },
     {
-      name: 'size',
-      title: 'Size (sq.m.)',
-      type: 'number',
-      validation: (rule) => rule.required().positive(),
+      name: 'owner',
+      title: 'Owner',
+      type: 'string',
+      validation: (rule) => rule.required()
     },
     {
-      name: 'image_description_sets',
-      title: 'Image and Description Sets',
+      name: 'mainImage1',
+      title: 'Main Image 1',
+      type: 'image',
+      options: {
+        hotspot: true,
+        accept: 'image/*',
+        storeOriginalFilename: true,
+      },
+      validation: (rule) => rule.required(),
+    },
+    {
+      name: 'mainImage2',
+      title: 'Main Image 2',
+      type: 'image',
+      options: {
+        hotspot: true,
+        accept: 'image/*',
+        storeOriginalFilename: true,
+      },
+      validation: (rule) => rule.required(),
+    },
+    {
+      name: 'description1',
+      title: 'Description 1',
+      type: 'text',
+    },
+    {
+      name: 'description2',
+      title: 'Description 2',
+      type: 'text',
+    },
+    {
+      name: 'description3',
+      title: 'Description 3',
+      type: 'text',
+    },
+    {
+      name: 'galleryImageSet',
+      title: 'Gallery Image Set',
+      type: 'array',
+      of: [{type: 'image'}],
+      options: {
+        layout: 'grid',
+        hotspot: true,
+        accept: 'image/*',
+        storeOriginalFilename: true,
+      },
+      validation: (rule) => rule.required(),
+    },
+    {
+      name: 'galleryImage1',
+      title: 'Gallery Image 1',
+      type: 'image',
+      options: {
+        hotspot: true,
+        accept: 'image/*',
+        storeOriginalFilename: true,
+      },
+    },
+    {
+      name: 'galleryImage2',
+      title: 'Gallery Image 2',
+      type: 'image',
+      options: {
+        hotspot: true,
+        accept: 'image/*',
+        storeOriginalFilename: true,
+      },
+    },
+    {
+      name: 'galleryImage3',
+      title: 'Gallery Image 3',
+      type: 'image',
+      options: {
+        hotspot: true,
+        accept: 'image/*',
+        storeOriginalFilename: true,
+      },
+    },
+    {
+      name: 'galleryImage4',
+      title: 'Gallery Image 4',
+      type: 'image',
+      options: {
+        hotspot: true,
+        accept: 'image/*',
+        storeOriginalFilename: true,
+      },
+    },
+    {
+      name: 'galleryImage5',
+      title: 'Gallery Image 5',
+      type: 'image',
+      options: {
+        hotspot: true,
+        accept: 'image/*',
+        storeOriginalFilename: true,
+      },
+    },
+    {
+      name: 'floorPlan',
+      title: 'Floor Plan',
       type: 'array',
       of: [
         {
           type: 'object',
           fields: [
             {
-              name: 'title',
+              name: 'floorLevel',
+              title: 'Floor Level',
+              type: 'number',
+              validation: (rule) => rule.required().positive(),
+            },
+            {
+              name: 'floorPlanTitle',
               title: 'Title',
               type: 'string',
-              validation: (rule) => rule.required(),
+              hidden: true,
+              readOnly: true,
             },
             {
-              name: 'description',
-              title: 'Description',
-              type: 'text',
-            },
-            {
-              name: 'image_sets',
-              title: 'Image Sets',
-              type: 'array',
-              of: [{type: 'image'}],
+              name: 'floorPlanImage',
+              title: 'Floor Plan Image',
+              type: 'image',
               options: {
-                layout: 'grid',
                 hotspot: true,
                 accept: 'image/*',
                 storeOriginalFilename: true,
@@ -89,39 +196,34 @@ export default {
               validation: (rule) => rule.required(),
             },
           ],
+          preview: {
+            select: {
+              title: 'floorLevel',
+              media: 'floorPlanImage',
+            },
+            prepare(selection) {
+              const { title, media } = selection;
+              return {
+                title: `Level ${title}`,
+                media: media,
+              };
+            },
+          },
         },
       ],
-    },
-    {
-      name: 'plan_title_image_sets',
-      title: 'Plan Title and Image Sets',
-      type: 'array',
-      of: [
-        {
-          type: 'object',
-          fields: [
-            {
-              name: 'title',
-              title: 'Title',
-              type: 'string',
-              validation: (rule) => rule.required(),
-            },
-            {
-              name: 'image_sets',
-              title: 'Image Sets',
-              type: 'array',
-              of: [{type: 'image'}],
-              options: {
-                layout: 'grid',
-                hotspot: true,
-                accept: 'image/*',
-                storeOriginalFilename: true,
-              },
-              validation: (rule) => rule.required(),
-            },
-          ],
-        },
-      ],
+      validation: (Rule) =>
+        Rule.custom((floorPlans) => {
+          if (!floorPlans) return true;
+
+          const levels = floorPlans.map((plan) => plan.floorLevel);
+          const uniqueLevels = new Set(levels);
+
+          if (uniqueLevels.size !== levels.length) {
+            return 'Each floor level must be unique.';
+          }
+
+          return true;
+        }),
     },
   ],
 }
