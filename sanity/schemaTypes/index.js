be55234c2493d9f79c85b31f8f86beef5ea333e2
Natthawut Saexu <xutiger2001@gmail.com>
2024-07-22 19:30:58 +0700

    🧑‍💻 Remove Author and Blog

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -1,7 +1,5 @@
 import portfolio from './portfolio.js'
-import blog from './blog.js'
 import catalogue from './catalogue.js'
-import author from './author.js'
 import news from './news.js'
 
-export const schemaTypes = [portfolio, blog, catalogue, author,news]
+export const schemaTypes = [portfolio,catalogue,news]
