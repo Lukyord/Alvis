38998e76753814d16a622ddcab0f2bb2417341be
Natthawut Saexu <xutiger2001@gmail.com>
2024-07-22 19:28:14 +0700

    ⏩ Revert to default sanity view

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-import {deskStructure} from './schemaTypes/deskStructure'
+
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +11,7 @@ export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_DATASET,
 
-  plugins: [structureTool({structure:deskStructure}), visionTool()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
